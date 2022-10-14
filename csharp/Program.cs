2697f7497d82e5dd161f914024784c31388917b4
mikayo0 <mikayo159@gmail.com>
2022-10-13 23:54:01 -0300

    caralho menor, um cavalo

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace csharp {
     internal class Program {
         static void Main(string[] args) {
+            Console.WriteLine("SECSOS");
+            Console.WriteLine("VAPO");
         }
     }
 }
